7a41378f8a48cd7af8d0e6bd261b10adb713cd34
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-12 17:46:20 -0300

    feat: Implement setup function for landing metadata table and update main execution flow

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import logging
 from pyspark.sql import SparkSession
 
-from config.paths import SHORTCUT_PATH
+from config.paths import (SHORTCUT_PATH, 
+                          LANDING_ROOT_PATH,
+                          LANDING_META_TABLE_NAME)
+
 from src.landing.extract_metadata import extract_metadata
 from src.landing.build_landing_path import build_landing_path
+from landing.setup_landing_metadata import setup_landing_metadata
 
 def main(): 
     
@@ -19,7 +23,12 @@ def main():
     df_metadata = extract_metadata(spark=spark,
                                    source_path=SHORTCUT_PATH) 
     
-    df_landing_path = build_landing_path(df=df_metadata)
+    df_landing_path = build_landing_path(df=df_metadata,
+                                         target_path=LANDING_ROOT_PATH)
+
+    df_meta_table = setup_landing_metadata(spark=spark, 
+                                           table_name=LANDING_META_TABLE_NAME)
 
     return (df_metadata,
-            df_landing_path)
+            df_landing_path,
+            df_meta_table)
