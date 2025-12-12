6ccfc121540ce9803f14dfed59841833283dfe68
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-12 13:47:12 -0300

    refactor: Simplify imports and improve schema definition by returning meta_schema

--- src/landing/define_landing_meta_schema.py
+++ src/landing/define_landing_meta_schema.py
@@ -1,16 +1,18 @@
 from pyspark.sql import functions as f
-from pyspark.sql.types import *
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, FloatType
 
 def define_landing_meta_schema() -> StructType:
     """  
     Define the schama for the landing meta table
     """
 
-    return StructType([
-        StructField('source_path', StringType(), False),
-        StructField('source_modified_at', TimestampType(), False),
-        StructField('source_size_mb', float(), False), 
+    meta_schema = StructType([
+        StructField('source_path', StringType(), False), 
+        StructField('source_modified_at', TimestampType(), False), 
+        StructField('source_size_mb', FloatType(), False), 
         StructField('bucket_path', StringType(), False), 
-        StructField('lakehouse_path', StringType(), False), 
-        StructField('copied_at', StringType(), False)
+        StructField('lakehouse_path', StringType(), False)
     ])
+
+    return meta_schema
+
