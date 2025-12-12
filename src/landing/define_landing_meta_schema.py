4136cce0578fdb075fc3644d4964cd2a762bf8c6
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-12 14:12:18 -0300

    feat: Implement functions to create and load the landing metadata table
    Functions:
    - _create_landing_meta_table
    - _get_meta_table

--- src/landing/define_landing_meta_schema.py
+++ src/landing/define_landing_meta_schema.py
@@ -1,7 +1,13 @@
+import logging
+from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql import functions as f
-from pyspark.sql.types import StructType, StructField, StringType, TimestampType, FloatType
+from pyspark.sql.types import (StructType, 
+                               StructField, 
+                               StringType, 
+                               TimestampType, 
+                               FloatType)
 
-def define_landing_meta_schema() -> StructType:
+def _define_meta_schema() -> StructType:
     """  
     Define the schama for the landing meta table
     """
@@ -16,3 +22,50 @@ def define_landing_meta_schema() -> StructType:
 
     return meta_schema
 
+def _create_meta_table (spark: SparkSession, 
+                        schema: StructType, 
+                        table_name: str) -> None: 
+    """  
+    Creates the landing metadata table if it does not already exist.
+    """
+    logging.info(
+        f'Ensuring landing metadata table exists: {table_name}'
+    )
+    try: 
+        (
+            spark.createDataFrame([], schema)
+            .write
+            .format('delta')
+            .mode('ignore')
+            .saveAsTable(table_name)
+        )
+        logging.info(
+            f'Landing metadata table {table_name} created or already exists'
+        )
+    except Exception as e: 
+        logging.exception(
+            f'Failed to create the landing meta table {table_name}', 
+            exc_info=True
+        )
+        raise e 
+    
+def _get_meta_table(spark: SparkSession, 
+                    table_name: str) -> DataFrame:
+    """  
+    Loads the landing metadata table as a Spark DataFrame
+    """
+    logging.info(
+        f'Loading landing metadata table {table_name}'
+        )
+    try: 
+        df_meta_table = spark.table(table_name)
+        logging.info (
+            'Successfully loaded landing metadata table {table_name}'
+        )
+        return df_meta_table
+    except Exception as e: 
+        logging.error(
+            f'Failed to load landing metadata table {table_name}',
+            exc_info=True
+        )
+        raise e
