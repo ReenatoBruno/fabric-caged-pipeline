cc252ad7b001f582313461ddec5cfcfe9b37332f
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-12 21:44:40 -0300

    fix: Update import and variable names for clarity in main execution flow

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from config.paths import (SHORTCUT_PATH,
                           LANDING_META_TABLE_NAME)
 
 from src.landing.extract_metadata import extract_metadata
-from src.landing.build_landing_path import build_landing_path
+from src.landing.build_landing_path import build_file_meta
 from landing.setup_landing_metadata import setup_landing_metadata
 
 def main(): 
@@ -25,14 +25,14 @@ def main():
                                    source_path=SHORTCUT_PATH) 
     display(df_metadata.limit(10))
     
-    df_landing_path = build_landing_path(df=df_metadata,
-                                         target_path=LANDING_ROOT_PATH)
-    display(df_landing_path.limit(10))
+    df_meta_columns = build_file_meta(df=df_metadata,
+                                      target_path=LANDING_ROOT_PATH)
+    display(df_meta_columns.limit(10))
 
     df_meta_table = setup_landing_metadata(spark=spark, 
                                            table_name=LANDING_META_TABLE_NAME)
-    display(df_meta_table .limit(10))
+    (df_meta_table .limit(10))
 
     return (df_metadata,
-            df_landing_path,
+            df_meta_columns,
             df_meta_table)
