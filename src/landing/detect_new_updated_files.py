6e2161a0665fe9805ea0ffd7afa1be7630d9d258
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-13 22:29:40 -0300

    feat: Add _get_new_or_updated_files function to identify new or updated files based on modification timestamps

--- src/landing/detect_new_updated_files.py
+++ src/landing/detect_new_updated_files.py
@@ -26,3 +26,25 @@ def _get_latest_copied_files(df: DataFrame) -> DataFrame:
         )
         raise e 
 
+def _get_new_or_updated_files(df_meta_table: DataFrame,
+                              df_latest_files: DataFrame) -> DataFrame: 
+    """  
+    Identifies new or updated files by comparing the current 'source_modified_at'
+    against the historic 'latest_copied_time'
+    """
+    logging.info(
+        'Identifying new or updated files based on modification timestamps'
+    )
+    df_files_to_copy = (
+        df_meta_table
+        .join(df_latest_files, on='source_path', how='left')
+        .filter(
+            f.col('source_modified_at') >
+            f.coalesce(
+                f.col('latest_copied_time'), 
+                f.lit('1970-01-01 00:00:00')
+                .cast('timestamp'))
+        )
+        .select('source_path', 'lakehouse_path')
+    )
+    return df_files_to_copy
