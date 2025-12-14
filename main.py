69476e66d4517f02d22430f89ed3c0e6e359efa4
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-13 23:42:04 -0300

    refactor: Update imports and pipeline calls after function renaming

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from config.paths import (SHORTCUT_PATH,
                           LANDING_META_TABLE_NAME)
 
 from src.landing.extract_metadata import extract_metadata
-from landing.build_meta_file import build_meta_file
+from landing.build_metadata import build_metadata
 from landing.setup_metadata_table import setup_meta_table
 
 def main(): 
@@ -21,18 +21,18 @@ def main():
         'Starting CAGED pipeline execution...'
     )
 
-    df_metadata = extract_metadata(spark=spark,
-                                   source_path=SHORTCUT_PATH) 
-    display(df_metadata.limit(10))
+    df_meta_extracted = extract_metadata(spark=spark,
+                                         source_path=SHORTCUT_PATH) 
+    display(df_meta_extracted.limit(10))
     
-    df_meta_columns = build_meta_file(df=df_metadata,
-                                      root_path=LANDING_ROOT_PATH)
-    display(df_meta_columns.limit(10))
+    df_metadata = build_metadata(df=df_meta_extracted,
+                                 root_path=LANDING_ROOT_PATH)
+    display(df_metadata.limit(10))
 
     df_meta_table = setup_meta_table(spark=spark, 
                                      table_name=LANDING_META_TABLE_NAME)
     (df_meta_table .limit(10))
 
-    return (df_metadata,
-            df_meta_columns,
+    return (df_meta_extracted,
+            df_metadata,
             df_meta_table)
