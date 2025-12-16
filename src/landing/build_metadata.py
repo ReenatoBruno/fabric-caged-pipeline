155faf95ac1b8519ddded46178a7744a72204b00
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-16 14:27:09 -0300

    refactor: Centralize logging and add top-level error handling to build_metadata function

--- src/landing/build_metadata.py
+++ src/landing/build_metadata.py
@@ -1,63 +1,32 @@
 import logging
 
-from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql import DataFrame
 from pyspark.sql import functions as f
 
 def _build_relative_bucket_path(df: DataFrame) -> DataFrame:
     """   
-    Build the relative bucket path within the OneLake
-    
-    Input'source_path': 
-    abfss://b6d448cc-4a6a-4928-b3d7-b9e1e3be481a@onelake.dfs.fabric.microsoft.com/
-    5b014b43-0a49-44a5-bc15-f25f51814631/Files/
-    amzn-caged-bucket-renato/caged/2020/CAGEDMOV202001.txt
-
-    Output 'bucket_path':
-    amzn-caged-bucket-renato/caged/2020/CAGEDMOV202001.txt
+    Adds a 'bucket_path' column to the DataFrame by extracting the relative 
+    file path from the 'source_path' column.
     """
-    logging.info(
-        'Building relative bucket path...'
-    )
-    try: 
-        df_with_bucket_path  = df.withColumn(
-            'bucket_path', 
-            f.expr("split(source_path, '/Files/', 2)[1]")
-        )
-        logging.info (
-            'Relative bucket path built successfully'
-        )
-        return df_with_bucket_path
-    except Exception as e: 
-        logging.exception(
-            'An error occurred while building the bucket path'
-        )
-        raise e 
+    
+    df_with_bucket_path  = df.withColumn(
+        'bucket_path', 
+        f.expr("split(source_path, '/Files/', 2)[1]")
+    )  
+    return df_with_bucket_path
 
 def _build_relative_lakehouse_path(df: DataFrame, 
                                    root_path: str) -> DataFrame:
     """
     Generates the logical lakehouse path by concatenating the root_path
     with the relative bucket_path.
-      
-    LANDING_ROOT_PATH = 'Files/Landing/CAGED'
     """
-    logging.info(
-        'Building logical lakehouse path...'
+
+    df_with_lakehouse_path = df.withColumn(
+        'lakehouse_path', 
+        f.concat(f.lit(root_path + '/'), f.col('bucket_path'))
     )
-    try:
-        df_with_lakehouse_path = df.withColumn(
-            'lakehouse_path', 
-            f.concat(f.lit(root_path + '/'), f.col('bucket_path'))
-        )
-        logging.info(
-            'Logical lakehouse path built successfully'
-        )
-        return df_with_lakehouse_path
-    except Exception as e:
-        logging.exception(
-            'An error occurred while building the logical lakehouse path'
-        )
-        raise e 
+    return df_with_lakehouse_path 
     
 def _select_meta_columns(df: DataFrame) -> DataFrame:
     """ 
@@ -83,11 +52,44 @@ def build_metadata(df: DataFrame,
     - lakehouse_path
     """
 
-    df_with_bucket_path = _build_relative_bucket_path(df=df)
-      
-    df_with_lakehouse_path = _build_relative_lakehouse_path(df=df_with_bucket_path, 
-                                                            root_path=root_path)
-      
-    df_metadata = _select_meta_columns(df=df_with_lakehouse_path)
+    try: 
+        logging.info(
+            'Starting metadata DataFrame build process'
+        )
 
-    return df_metadata
+        logging.info(
+            'Building relative bucket path from source_path.'
+        )
+        df_with_bucket_path = _build_relative_bucket_path(df=df)
+        logging.info(
+            'Relative bucket path built successfully'
+        )
+        
+        logging.info(
+            'Building relative lakehouse path (target destination)'
+        )
+        df_with_lakehouse_path = _build_relative_lakehouse_path(df=df_with_bucket_path, 
+                                                                root_path=root_path)
+        logging.info(
+            'Relative lakehouse path built successfully'
+        )
+
+        logging.info(
+            'Selecting final set of metadata columns.'
+        )
+        df_metadata = _select_meta_columns(df=df_with_lakehouse_path)
+        logging.info(
+            'Final metadata columns selected'
+        )
+
+        logging.info(
+            'Metadata DataFrame build process completed'
+        )
+
+        return df_metadata
+    except Exception as e:
+        logging.error(
+            f'FATAL ERROR in {build_metadata.__name__}: Metadata DataFrame building failed.',
+            exc_info=True
+        )
+        raise
