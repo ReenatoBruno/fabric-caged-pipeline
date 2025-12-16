56c62a5e6595cfc105ffab8f973e74a2e098b521
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-16 12:26:28 -0300

    refactor: Improve function clarity and maintainability for metadata calculation
    Removes immediate processing (logging) from the core logic, which improves the function's readability.

--- src/landing/orchestrate_incremental_copy.py
+++ src/landing/orchestrate_incremental_copy.py
@@ -8,25 +8,13 @@ def _get_latest_copied_files(df: DataFrame) -> DataFrame:
     """
     Returns the most recent modification timestamp for each source file.
     """
-    logging.info(
-        'Starting computation of latest copied files.'
+    
+    df_latest_files = (
+        df.groupBy('source_path')
+        .agg(f.max('source_modified_at')
+             .alias('latest_copied_time'))
     )
-    try: 
-        df_latest_files = (
-            df.groupBy('source_path')
-            .agg(f.max('source_modified_at')
-                 .alias('latest_copied_time'))
-        )
-        logging.info(
-            'Successfully computed latest copied timestamps'
-        )
-        return df_latest_files
-    except Exception as e:
-        logging.error(
-            'Error while computing latest copied files.', 
-            exc_info=True
-        )
-    raise
+    return df_latest_files
 
 def _get_new_or_updated_files(df_metadata: DataFrame,
                               df_latest_files: DataFrame) -> DataFrame: 
