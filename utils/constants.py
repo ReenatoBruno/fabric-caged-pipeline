3b12356156ccecf27ff72f1b2608dbd8f6e62099
ReenatoBruno <ReenatoBruno@users.noreply.github.com>
2025-12-18 20:13:44 -0300

    feat: add MAX_FILES_ALLOWED guardrail for driver-based file copy

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,8 @@
 
-
 # Number of bytes in one mebibyte (MiB).
 #   - Used for converting file sizes from bytes to MB
 #   - Helps avoid magic numbers in the code
 BYTES_PER_MB = 1024 * 1024
+
+# Guardrail to avoid OOM and long runtimes when copying files on the Driver (Fabric)
+MAX_FILES_ALLOWED = 500 
